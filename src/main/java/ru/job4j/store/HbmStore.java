794ec1f535f4c4c2ff46b6052393ba209cdcd5e9
iudini <hoiitzx@gmail.com>
2021-07-29 19:26:10 +0300

    3. Лямбды и шаблон wrapper. [#49295]

--- src/main/java/ru/job4j/store/HbmStore.java
+++ src/main/java/ru/job4j/store/HbmStore.java
@@ -2,13 +2,15 @@ package ru.job4j.store;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.model.Item;
 
+import javax.persistence.Query;
 import java.util.Collection;
-import java.util.List;
+import java.util.function.Function;
 
 public class HbmStore implements Store, AutoCloseable {
     private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
@@ -26,37 +28,43 @@ public class HbmStore implements Store, AutoCloseable {
 
     @Override
     public Collection<Item> findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> list = session.createQuery("from ru.job4j.model.Item order by done, created").list();
-        session.getTransaction().commit();
-        session.close();
-        return list;
+        return this.tx(session -> session.createQuery("from Item order by done, created").list());
     }
 
     @Override
     public void update(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item itemWithId = (Item) session.createQuery("from ru.job4j.model.Item where id=" + item.getId())
-                .getSingleResult();
-        itemWithId.setDone(true);
-        session.update(itemWithId);
-        session.getTransaction().commit();
-        session.close();
+        this.tx(session -> {
+           final Query query = session.createQuery("from Item where id=:id");
+           query.setParameter("id", item.getId());
+           Item itemWithId = (Item) query.getSingleResult();
+           itemWithId.setDone(true);
+           session.update(itemWithId);
+           return null;
+        });
     }
 
     @Override
     public void save(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
+        tx(session -> session.save(item));
     }
 
     @Override
     public void close() throws Exception {
         StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 }
